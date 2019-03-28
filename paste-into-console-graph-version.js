58448cbd505d564e2e3d067eb11e5a6ede3391ad
K. Adam White <kadamwhite@users.noreply.github.com>
2019-03-28 13:42:43 -0400

    Eslint conformance

--- paste-into-console-graph-version.js
+++ paste-into-console-graph-version.js
@@ -1,168 +1,171 @@
-var { convert, svgify } = (() => {
-function sum( collection, prop ) {
-  return collection.reduce( ( sum, item ) => sum + item[prop], 0 );
-}
+let { convert, svgify } = ( () => {
+	function sum( collection, prop ) {
+		return collection.reduce( ( sum, item ) => sum + item[prop], 0 );
+	}
 
-function testEq( a, b ) {
-  if ( a !== b ) {
-    throw new Error( `Expected ${ a } to equal ${ b }` );
-  }
-}
+	function testEq( a, b ) {
+		if ( a !== b ) {
+			throw new Error( `Expected ${ a } to equal ${ b }` );
+		}
+	}
 
-testEq( 8, sum( [ { a: 1 }, { a: 4 }, { a: 3 } ], 'a' ) );
+	testEq( 8, sum( [ { a: 1 }, { a: 4 }, { a: 3 } ], 'a' ) );
 
-const distance = ( pt1, pt2 ) => Math.sqrt(
-  ( Math.abs( pt1.x - pt2.x ) ** 2 ) + ( Math.abs( pt1.y - pt2.y ) ** 2 )
-);
+	const distance = ( pt1, pt2 ) => Math.sqrt(
+		( Math.abs( pt1.x - pt2.x ) ** 2 ) + ( Math.abs( pt1.y - pt2.y ) ** 2 )
+	);
 
-const randomSampleArray = ( arr, count ) => {
-  if ( count >= arr.length ) {
-    return arr;
-  }
-  const clonedArr = [ ...arr ];
-  const randomElements = [];
-  while ( randomElements.length < count ) {
-    const idx = Math.floor( Math.random() * clonedArr.length );
-    randomElements.push( clonedArr.splice( idx, 1 )[ 0 ] );
-  }
-  return randomElements;
-}
+	const randomSampleArray = ( arr, count ) => {
+		if ( count >= arr.length ) {
+			return arr;
+		}
+		const clonedArr = [ ...arr ];
+		const randomElements = [];
+		while ( randomElements.length < count ) {
+			const idx = Math.floor( Math.random() * clonedArr.length );
+			randomElements.push( clonedArr.splice( idx, 1 )[ 0 ] );
+		}
+		return randomElements;
+	}
 
-const omitList = [
-  'NOSCRIPT',
-  'SCRIPT',
-  'LINK',
-  'OPTION',
-  'STYLE',
-];
+	const omitList = [
+		'NOSCRIPT',
+		'SCRIPT',
+		'LINK',
+		'OPTION',
+		'STYLE',
+	];
 
-const isOutOfBounds = ( boundingBox, point ) => (
-  point.x < boundingBox.x ||
+	const isOutOfBounds = ( boundingBox, point ) => (
+		point.x < boundingBox.x ||
   point.x > boundingBox.x + boundingBox.width ||
   point.y < boundingBox.y ||
   point.y > boundingBox.y + boundingBox.height
-);
+	);
 
-const toPoint = ( x, y ) => ( { x, y } );
+	const toPoint = ( x, y ) => ( {
+		x,
+		y,
+	} );
 
-const ascNumericSort = ( a, b ) => a - b;
-const pointsToLine = ( p1, p2 ) => {
-  const sortedPoints = [ p1, p2 ].sort( ( a, b ) => ascNumericSort( a.x * a.y, b.x * b.y ) );
-  const x1 = sortedPoints[0].x;
-  const y1 = sortedPoints[0].y;
-  const x2 = sortedPoints[1].x;
-  const y2 = sortedPoints[1].y;
-  const style = 'style="fill:none;stroke:black;stroke-width:1;"';
-  return `<line x1="${ x1 }" y1="${ y1 }" x2="${ x2 }" y2="${ y2 }" ${ style } />`;
-};
+	const ascNumericSort = ( a, b ) => a - b;
+	const pointsToLine = ( p1, p2 ) => {
+		const sortedPoints = [ p1, p2 ].sort( ( a, b ) => ascNumericSort( a.x * a.y, b.x * b.y ) );
+		const x1 = sortedPoints[0].x;
+		const y1 = sortedPoints[0].y;
+		const x2 = sortedPoints[1].x;
+		const y2 = sortedPoints[1].y;
+		const style = 'style="fill:none;stroke:black;stroke-width:1;"';
+		return `<line x1="${ x1 }" y1="${ y1 }" x2="${ x2 }" y2="${ y2 }" ${ style } />`;
+	};
 
-const pointToCircle = ( point ) => {
-  const { x, y } = point;
-  return `<circle cx="${ x }" cy="${ y }" r="2" fill="none" stroke="black" />`;
-};
-const addIfUnique = ( elements, element ) => {
-  if ( ! elements.includes( element ) ) {
-    elements.push( element );
-  }
-};
+	const pointToCircle = point => {
+		const { x, y } = point;
+		return `<circle cx="${ x }" cy="${ y }" r="2" fill="none" stroke="black" />`;
+	};
+	const addIfUnique = ( elements, element ) => {
+		if ( ! elements.includes( element ) ) {
+			elements.push( element );
+		}
+	};
 
-const nextFrame = () => new Promise( resolve => setTimeout( resolve, 0 ) );
+	const nextFrame = () => new Promise( resolve => setTimeout( resolve, 0 ) );
 
-const injectProgressIndicator = () => {
-  let indicator = document.getElementById( 'progress-indicator' );
-  if ( indicator ) {
-    return indicator;
-  }
-  indicator = document.createElement( 'script' );
-  indicator.id = 'progress-indicator';
-  indicator.type = 'text/html';
-  indicator.setAttribute( 'data-progress', '0%' );
-  document.body.append( indicator );
-  return indicator;
-}
+	const injectProgressIndicator = () => {
+		let indicator = document.getElementById( 'progress-indicator' );
+		if ( indicator ) {
+			return indicator;
+		}
+		indicator = document.createElement( 'script' );
+		indicator.id = 'progress-indicator';
+		indicator.type = 'text/html';
+		indicator.setAttribute( 'data-progress', '0%' );
+		document.body.append( indicator );
+		return indicator;
+	}
 
-const nodeToSVG = async container => {
-  const indicator = injectProgressIndicator();
-  const boundingBox = container.getBoundingClientRect();
+	const nodeToSVG = async container => {
+		const indicator = injectProgressIndicator();
+		const boundingBox = container.getBoundingClientRect();
 
-  const maxDiagonal = distance( toPoint( 0, 0 ), toPoint( boundingBox.width, boundingBox.height ) );
-  // const minLineLength = 0.05 * maxDiagonal;
-  // const maxLineLength = 0.15 * maxDiagonal;
-  let minLineLength = Math.min(
-    0.05 * maxDiagonal,
-    0.1 * boundingBox.width
-  );
-  let maxLineLength = Math.min(
-    0.10 * maxDiagonal,
-    0.25 * boundingBox.width
-  );
-  if ( maxLineLength < minLineLength ) {
-    // See https://medium.com/@frontman/how-swap-two-values-without-temporary-variables-using-javascript-8bb28f96b5f6
-    maxLineLength = minLineLength + ( minLineLength = maxLineLength, 0 );
-  }
+		const maxDiagonal = distance( toPoint( 0, 0 ), toPoint( boundingBox.width, boundingBox.height ) );
+		// const minLineLength = 0.05 * maxDiagonal;
+		// const maxLineLength = 0.15 * maxDiagonal;
+		let minLineLength = Math.min(
+			0.05 * maxDiagonal,
+			0.1 * boundingBox.width
+		);
+		let maxLineLength = Math.min(
+			0.10 * maxDiagonal,
+			0.25 * boundingBox.width
+		);
+		if ( maxLineLength < minLineLength ) {
+			// See https://medium.com/@frontman/how-swap-two-values-without-temporary-variables-using-javascript-8bb28f96b5f6
+			maxLineLength = minLineLength + ( minLineLength = maxLineLength, 0 );
+		}
 
-  const points = [ ...container.querySelectorAll( '*' ) ]
-    .map( node => {
-      if ( omitList.includes( node.tagName ) ) {
-        return null;
-      }
-      const { x, y, width, height } = node.getBoundingClientRect();
-      const cx = x + width / 2;
-      const cy = y + height / 2;
-      if ( cx === 0 && cy === 0 ) {
-        return null;
-      }
-      return {
-        x: cx,
-        y: cy,
-        count: 0,
-      };
-    } )
-    .filter( Boolean )
-    .filter( point => ! isOutOfBounds( boundingBox, point ) );
+		const points = [ ...container.querySelectorAll( '*' ) ]
+			.map( node => {
+				if ( omitList.includes( node.tagName ) ) {
+					return null;
+				}
+				const { x, y, width, height } = node.getBoundingClientRect();
+				const cx = x + width / 2;
+				const cy = y + height / 2;
+				if ( cx === 0 && cy === 0 ) {
+					return null;
+				}
+				return {
+					x: cx,
+					y: cy,
+					count: 0,
+				};
+			} )
+			.filter( Boolean )
+			.filter( point => ! isOutOfBounds( boundingBox, point ) );
 
-  const increment = 1 / points.length;
-  let progress = 0;
+		const increment = 1 / points.length;
+		let progress = 0;
 
-  const svgElements = [];
-  let currentIdx = 0;
-  for ( let point of points ) {
-    addIfUnique( svgElements, pointToCircle( point ) );
+		const svgElements = [];
+		let currentIdx = 0;
+		for ( let point of points ) {
+			addIfUnique( svgElements, pointToCircle( point ) );
 
-    let batchSize = 100;
-    const remainingPoints = points
-      .slice( currentIdx + 1 )
-      // No vertical or horizontal lines
-      .filter( ( { x, y } ) => {
-        if ( x === point.x || y === point.y ) {
-          return false;
-        }
-        return true;
-      } )
-      // No more than three lines to any one point
-      .filter( point2 => point2.count <= 3 )
-      .filter( point2 => {
-        const dist = distance( point, point2 );
-        return dist > minLineLength && dist < maxLineLength;
-      } );
+			let batchSize = 100;
+			const remainingPoints = points
+				.slice( currentIdx + 1 )
+			// No vertical or horizontal lines
+				.filter( ( { x, y } ) => {
+					if ( x === point.x || y === point.y ) {
+						return false;
+					}
+					return true;
+				} )
+			// No more than three lines to any one point
+				.filter( point2 => point2.count <= 3 )
+				.filter( point2 => {
+					const dist = distance( point, point2 );
+					return dist > minLineLength && dist < maxLineLength;
+				} );
 
-    for ( let point2 of randomSampleArray( remainingPoints, 1 ) ) {
-    // for ( let point2 of randomSampleArray( remainingPoints, 4 ) ) {
-      point2.count = point2.count ? point2.count + 1 : 1;
-      addIfUnique( svgElements, pointsToLine( point, point2 ) );
-      batchSize -= 1;
-      if ( batchSize < 0 ) {
-        batchSize = 100;
-        await nextFrame();
-      }
-    }
+			for ( let point2 of randomSampleArray( remainingPoints, 1 ) ) {
+				// for ( let point2 of randomSampleArray( remainingPoints, 4 ) ) {
+				point2.count = point2.count ? point2.count + 1 : 1;
+				addIfUnique( svgElements, pointsToLine( point, point2 ) );
+				batchSize -= 1;
+				if ( batchSize < 0 ) {
+					batchSize = 100;
+					await nextFrame();
+				}
+			}
 
-    currentIdx += 1;
-    progress = progress + increment;
-    indicator.setAttribute( 'data-progress', `${ parseInt( progress * 100, 10 ) }%` );
-  }
+			currentIdx += 1;
+			progress = progress + increment;
+			indicator.setAttribute( 'data-progress', `${ parseInt( progress * 100, 10 ) }%` );
+		}
 
-  return `
+		return `
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <svg
   xmlns:svg="http://www.w3.org/2000/svg"
@@ -171,19 +174,18 @@ const nodeToSVG = async container => {
 >
   ${ svgElements.map( el => `\t${ el }` ).join( '\n' ) }
 </svg>`;
-};
+	};
 
-
-return { convert: nodeToSVG };
-})();
+	return { convert: nodeToSVG };
+} )();
 
 console.time( 'svg created in' );
 convert( document.body )
-  .then(
-    svg => {
-      console.timeEnd( 'svg created in' );
-      console.log( 'run `copy( svg )` to pull SVG content into clipboard.' );
-      window.svg = svg;
-    },
-    err => console.error( err )
-  );
+	.then(
+		svg => {
+			console.timeEnd( 'svg created in' );
+			console.log( 'run `copy( svg )` to pull SVG content into clipboard.' );
+			window.svg = svg;
+		},
+		err => console.error( err )
+	);
--- paste-into-console.js
+++ paste-into-console.js
@@ -1,70 +1,70 @@
-var { convert, svgify } = (() => {
-function sum( collection, prop ) {
-  return collection.reduce( ( sum, item ) => sum + item[prop], 0 );
-}
+let { convert, svgify } = ( () => {
+	function sum( collection, prop ) {
+		return collection.reduce( ( sum, item ) => sum + item[prop], 0 );
+	}
 
-function testEq( a, b ) {
-  if ( a !== b ) {
-    throw new Error( `Expected ${ a } to equal ${ b }` );
-  }
-}
+	function testEq( a, b ) {
+		if ( a !== b ) {
+			throw new Error( `Expected ${ a } to equal ${ b }` );
+		}
+	}
 
-testEq( 8, sum( [ { a: 1 }, { a: 4 }, { a: 3 } ], 'a' ) );
+	testEq( 8, sum( [ { a: 1 }, { a: 4 }, { a: 3 } ], 'a' ) );
 
-const omitList = [
-  'IFRAME',
-  'NOSCRIPT',
-  'SCRIPT',
-];
+	const omitList = [
+		'IFRAME',
+		'NOSCRIPT',
+		'SCRIPT',
+	];
 
-const convertNode = ( node ) => {
-  if ( omitList.includes( node.tagName ) ) {
-    node.remove();
-    return null;
-  }
-  const children = (
-    // Treat SVG as one unit
-    node.tagName !== 'SVG'
-  ) && (
-    // Must have child nodes
-    node.children && node.children.length
-  ) ?
-    [ ...node.children ].map( convertNode ).filter( Boolean ) :
-    null;
-  return {
-    type: node.nodeType,
-    tag: node.tagName,
-    size: children ?
-      Math.ceil( Math.sqrt( sum( children, 'size' ) ) ) :
-      1,
-    children,
-  };
-};
+	const convertNode = node => {
+		if ( omitList.includes( node.tagName ) ) {
+			node.remove();
+			return null;
+		}
+		const children = (
+		// Treat SVG as one unit
+			node.tagName !== 'SVG'
+		) && (
+		// Must have child nodes
+			node.children && node.children.length
+		) ?
+			[ ...node.children ].map( convertNode ).filter( Boolean ) :
+			null;
+		return {
+			type: node.nodeType,
+			tag: node.tagName,
+			size: children ?
+				Math.ceil( Math.sqrt( sum( children, 'size' ) ) ) :
+				1,
+			children,
+		};
+	};
 
-const nodeToHTML = node => {
-  const el = document.createElement( node.tag );
-  el.innerHTML = node.children ?
-  node.children.map( nodeToHTML ).join( '\n' ) :
-  '';;
-  if ( node.size > 1 ) {
-    el.style.width = `${ node.size }em !important`;
-    el.style.height = `${ node.size }em !important`;
-  } else {
-    el.style.width = '0.1em !important';
-    el.style.height = '0.1em !important';
-  }
-  return el.outerHTML;
-};
+	const nodeToHTML = node => {
+		const el = document.createElement( node.tag );
+		el.innerHTML = node.children ?
+			node.children.map( nodeToHTML ).join( '\n' ) :
+			'';
+		if ( node.size > 1 ) {
+			el.style.width = `${ node.size }em !important`;
+			el.style.height = `${ node.size }em !important`;
+		} else {
+			el.style.width = '0.1em !important';
+			el.style.height = '0.1em !important';
+		}
+		return el.outerHTML;
+	};
 
-[ ...document.head.children ].forEach( node => {
-  if ( [ 'LINK', 'SCRIPT', 'STYLE' ].includes( node.tagName ) ) {
-    node.remove();
-  }
-} );
+	[ ...document.head.children ].forEach( node => {
+		if ( [ 'LINK', 'SCRIPT', 'STYLE' ].includes( node.tagName ) ) {
+			node.remove();
+		}
+	} );
 
-const styleNode = document.createElement( 'style' );
-styleNode.type = 'text/css';
-styleNode.innerHTML = `
+	const styleNode = document.createElement( 'style' );
+	styleNode.type = 'text/css';
+	styleNode.innerHTML = `
 div, time,
 svg, canvas, img, figure, figcaption,
 p, a, h1, h2, h3, h4, h5, h6,
@@ -92,26 +92,29 @@ img, svg, canvas, input, textarea {
   height: 1rem !important;
 }
 `;
-// p, li, a, span {
-//   height: 0.5em !important;
-// }
-document.head.append( styleNode );
+	// p, li, a, span {
+	//   height: 0.5em !important;
+	// }
+	document.head.append( styleNode );
 
-function svgify( node ) {
-  const rects = [];
-  const style = 'style="fill:none;stroke:#000000;"';
-  const max = { x: 0, y: 0 };
-  function svgifyNode( node ) {
-    const { x, top: y, width, height } = node.getBoundingClientRect();
-    max.x = Math.max( max.x, x + width );
-    max.y = Math.max( max.y, y + height );
-    rects.push(
-      `<rect ${ style } x="${ x }" y="${ y }" width="${ width }" height="${ height }" />`
-    );
-    [ ...node.children ].forEach( svgifyNode );
-  }
-  svgifyNode( node );
-  return `
+	function svgify( node ) {
+		const rects = [];
+		const style = 'style="fill:none;stroke:#000000;"';
+		const max = {
+			x: 0,
+			y: 0,
+		};
+		function svgifyNode( node ) {
+			const { x, top: y, width, height } = node.getBoundingClientRect();
+			max.x = Math.max( max.x, x + width );
+			max.y = Math.max( max.y, y + height );
+			rects.push(
+				`<rect ${ style } x="${ x }" y="${ y }" width="${ width }" height="${ height }" />`
+			);
+			[ ...node.children ].forEach( svgifyNode );
+		}
+		svgifyNode( node );
+		return `
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <svg
   xmlns:svg="http://www.w3.org/2000/svg"
@@ -120,15 +123,15 @@ function svgify( node ) {
 >
   ${ rects.join( '\n' ) }
 </svg>`;
-}
+	}
 
-return {
-  convert: function( node ) {
-    node.outerHTML = nodeToHTML( convertNode( node ) );
-  },
-  svgify,
-};
-})();
+	return {
+		convert: function ( node ) {
+			node.outerHTML = nodeToHTML( convertNode( node ) );
+		},
+		svgify,
+	};
+} )();
 
 convert( document.body );
 copy( svgify( document.body ) );
